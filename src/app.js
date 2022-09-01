1103f895c02d4747edf121bbb4b822c8bc802567
Vinícius Barros <vicaetano_barros@hotmail.com>
2022-09-01 20:09:02 -0300

    updated the logic

--- src/app.js
+++ src/app.js
@@ -17,4 +17,42 @@ client.connect().then(() => {
   db = client.db("batepapouol");
 });
 
+app.get("/participants", async (req, res) => {
+  try {
+    const users = await db.collection("teste1").find().toArray();
+    res.send(users);
+  } catch (error) {
+    res.sendStatus(500);
+  }
+});
+
+app.post("/participants", async (req, res) => {
+  const { name } = req.body;
+  try {
+    const user = await db.collection("teste1").insertOne({
+      name,
+      lastStatus: Date.now(),
+    });
+    res.sendStatus(201);
+  } catch (error) {
+    res.sendStatus(422);
+  }
+});
+
+app.get("/messages", async (req, res) => {
+  const { limit } = req.query;
+  const { user } = req.headers;
+  let aux = 0;
+  if (limit < 0) limit = aux;
+  try {
+    const messages = await db.collection("teste2").find().toArray();
+    const toUserMessages = messages.filter((msg) => {
+      msg.to === user || msg.type === "message" || msg.from === user;
+    });
+    res.send(toUserMessages.slice(-limit));
+  } catch (err) {
+    res.sendStatus(409);
+  }
+});
+
 app.listen(5000);
